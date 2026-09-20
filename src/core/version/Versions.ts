3595a27319c9b35f52977814db67a1cad4e2f3c1
bersavesab-hue <bersavesab@gmail.com>
2026-09-20 23:03:57 +0800

    chore: bump stage 15 used stock content version

--- src/core/version/Versions.ts
+++ src/core/version/Versions.ts
@@ -1,3 +1,3 @@
-export const GAME_VERSION = "0.15.7-stage15-newstock";
+export const GAME_VERSION = "0.15.8-stage15-usedstock";
 export const SAVE_VERSION = 1;
-export const CONTENT_VERSION = 8;
+export const CONTENT_VERSION = 9;
