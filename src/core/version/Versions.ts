c5f88d02eb35d8db3ede2858aecf5543846bd0d6
bersavesab-hue <bersavesab@gmail.com>
2026-09-23 02:39:14 +0800

    stage17: refine route overlays and city labels

--- src/core/version/Versions.ts
+++ src/core/version/Versions.ts
@@ -1,3 +1,3 @@
-export const GAME_VERSION = "0.19.2-stage17-road-visual-master";
+export const GAME_VERSION = "0.19.3-stage17-route-city-labels";
 export const SAVE_VERSION = 1;
 export const CONTENT_VERSION = 10;
--- src/presentation/android/PlayableClient.ts
+++ src/presentation/android/PlayableClient.ts
@@ -94,7 +94,7 @@ class PlayableClient {
       this.runtime.repositories.allServicePlans();
 
     return {
-      version: "0.19.2-road-visual-master",
+      version: "0.19.3-route-city-labels",
       company,
       currentGameSecond:
         Number(this.currentGameSecond),
