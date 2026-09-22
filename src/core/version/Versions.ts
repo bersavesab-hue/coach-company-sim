0288e92808cb2e341c8c12dd7aee379b987b6ca3
bersavesab-hue <bersavesab@gmail.com>
2026-09-23 00:35:11 +0800

    stage17: polish navigation road rendering

--- src/core/version/Versions.ts
+++ src/core/version/Versions.ts
@@ -1,3 +1,3 @@
-export const GAME_VERSION = "0.18.9-stage17-road-render-polish";
+export const GAME_VERSION = "0.18.9-stage17-road-skin";
 export const SAVE_VERSION = 1;
 export const CONTENT_VERSION = 10;
